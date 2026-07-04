5bd03f1ba2b419c7966d40ff60ecfc274e051a75
brezel-person <z.college42@gmail.com>
2026-07-04 01:11:38 +0000

    First commit of working website

---
+++ script.js
@@ -0,0 +1,55 @@
+const btn1 = document.getElementById("quiz1_btn");
+const btn2 = document.getElementById("quiz2_btn");
+const btn3 = document.getElementById("quiz3_btn");
+const btn4 = document.getElementById("quiz4_btn");
+const scarybtn = document.getElementById("scary_btn");
+const homebtn = document.getElementById("home_btn");
+let quizNum;
+
+if(homebtn){
+    homebtn.addEventListener("click", function(){
+        window.location.href="main.html";
+    });
+}
+
+if(btn1){
+    btn1.addEventListener("click", function(){
+        window.location.href= "quiz.html?quiz=1";
+    });
+    btn2.addEventListener("click", function(){
+        window.location.href= "info.html";
+    });
+    btn3.addEventListener("click", function(){
+        window.location.href= "learncrochet.html";
+    });
+    btn4.addEventListener("click", function(){
+        window.location.href= "quiz.html?quiz=2";
+    });
+    scarybtn.addEventListener("click", function(){
+        window.location.href= "scaryquiz.html";
+    });
+}
+
+$(document).ready(function(){
+    var handsOnHobbyPopUp = new bootstrap.Modal($('#handsOnHobbyPopUp')[0]);
+
+    $('#hands-on-hobby-yes').click(() => handsOnHobbyPopUp.show());
+    $('#creative-yes').click(() => handsOnHobbyPopUp.show());
+    $('#activity-yes').click(() => handsOnHobbyPopUp.show());
+    $('#patient-yes').click(() => handsOnHobbyPopUp.show());
+});
+
+$(document).ready(function(){
+    var maybePopUp = new bootstrap.Modal($('#maybePopUp')[0]);
+    $('#creative-maybe').click(() => maybePopUp.show());
+    $('#patient-maybe').click(() => maybePopUp.show());
+});
+
+$(document).ready(function(){
+    var nopePopUp = new bootstrap.Modal($('#nopePopUp')[0]);
+
+    $('#hands-on-hobby-no').click(() => nopePopUp.show());
+    $('#creative-no').click(() => nopePopUp.show());
+    $('#activity-no').click(() => nopePopUp.show());
+    $('#patient-no').click(() => nopePopUp.show());
+});
